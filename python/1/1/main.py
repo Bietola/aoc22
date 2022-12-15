1d3481cf531a3d9c72aa5b6a90d30c021db3a4f2
Bietola <dincio.montesi@gmail.com>
2022-12-15 09:23:48 +0100

    Compact solution

--- python/1/1/main.py
+++ python/1/1/main.py
@@ -1,24 +1,23 @@
 from fileinput import input
 from itertools import accumulate, groupby, chain
-from functools import reduce
 import math
 
-print(sum(map(
-    lambda x: (math.ceil((x[0]-20)/40)+0.5)*40 * x[1],
-    list(map(lambda x: x[-1], map(
-        lambda x: list(x[1]),
-        groupby(
-            accumulate(
-                chain([(1, 1)], map(
-                    lambda x: next(
-                        (2, int(x[1])) if x[0] == 'addx' else (1, 0)
-                        for x in [x.strip().split()]
-                    ),
-                    list(input()))
+print(sum(list(map(
+    lambda x: next(
+        (math.ceil((x[0]-20)/40)+0.5)*40 * x[1]
+        for x in [list(x[1])[-1]]
+    ),
+    groupby(
+        accumulate(
+            chain([(1, 1)], map(
+                lambda x: next(
+                    (2, int(x[1])) if x[0] == 'addx' else (1, 0)
+                    for x in [x.strip().split()]
                 ),
-                lambda s, x: (s[0] + x[0], s[1] + x[1])
-            ),
-            lambda x: (x[0] - 21) // 40
-        )
-    )))[:-1]
-)))
+                list(input())
+            )),
+            lambda s, x: (s[0] + x[0], s[1] + x[1])
+        ),
+        lambda x: (x[0] - 21) // 40
+    )
+))[:-1]))
