43e0ee708016a2db5af377b297a3c718acd50568
Bietola <dincio.montesi@gmail.com>
2022-12-16 13:12:02 +0100

    Remove comments

--- python/1/1/defaultdict.py
+++ python/1/1/defaultdict.py
@@ -4,12 +4,6 @@ from itertools import accumulate, groupby, chain, count, takewhile
 from more_itertools import take
 import math
 
-# d = defaultdict(lambda: None, {0: 5})
-# f = lambda k: d[k] if d[k] else f(k - 1)
-# print(f(10))
-
-# print(list(take(20, count(20, 40))))
-
 d = defaultdict(lambda: None,
     accumulate(
         chain(
@@ -31,23 +25,3 @@ print(sum(map(
     lambda x: (math.ceil((x[0]-20)/40)+0.5)*40 * x[1],
     takewhile(lambda x: x[1] < 1024, (f(x) for x in count(20, 40)))
 )))
-
-# print(sum(list(map(
-#     lambda x: next(
-#         (math.ceil((x[0]-20)/40)+0.5)*40 * x[1]
-#         for x in [list(x[1])[-1]]
-#     ),
-#     groupby(
-#         accumulate(
-#             chain([(1, 1)], map(
-#                 lambda x: next(
-#                     (2, int(x[1])) if x[0] == 'addx' else (1, 0)
-#                     for x in [x.strip().split()]
-#                 ),
-#                 list(input())
-#             )),
-#             lambda s, x: (s[0] + x[0], s[1] + x[1])
-#         ),
-#         lambda x: (x[0] - 21) // 40
-#     )
-# ))[:-1]))
