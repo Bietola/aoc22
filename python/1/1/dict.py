33b97f8584b612d0d9e372b0a17b5182eaf5a038
Bietola <dincio.montesi@gmail.com>
2022-12-16 14:59:07 +0100

    defaultdict -> dict

--- python/1/1/dict.py
+++ python/1/1/dict.py
@@ -1,10 +1,8 @@
 from fileinput import input
-from collections import defaultdict
-from itertools import accumulate, groupby, chain, count, takewhile
-from more_itertools import take
+from itertools import accumulate, chain, count, takewhile
 import math
 
-d = defaultdict(lambda: None,
+d = dict(
     accumulate(
         chain(
             [(1, 1)],
@@ -20,7 +18,7 @@ d = defaultdict(lambda: None,
         lambda s, x: (s[0] + x[0], s[1] + x[1])
     )
 )
-f = lambda k: (k, d[k]) if d[k] else f(k - 1)
+f = lambda k: (k, d[k]) if d.get(k) else f(k - 1)
 print(sum(map(
     lambda x: (math.ceil((x[0]-20)/40)+0.5)*40 * x[1],
     takewhile(lambda x: x[1] < 1024, (f(x) for x in count(20, 40)))
