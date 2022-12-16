805075f06744c920f351e641ee651c6ffe84d2ac
Bietola <dincio.montesi@gmail.com>
2022-12-16 13:11:41 +0100

    Solve with dict method

--- python/1/1/default-dict-forbidden-assign.py
+++ python/1/1/default-dict-forbidden-assign.py
@@ -1,6 +1,6 @@
 from fileinput import input
 from collections import defaultdict
-from itertools import accumulate, groupby, chain, count, takewhile
+from itertools import accumulate, groupby, chain, count
 from more_itertools import take
 import math
 
@@ -10,27 +10,27 @@ import math
 
 # print(list(take(20, count(20, 40))))
 
-d = defaultdict(lambda: None,
-    accumulate(
-        chain(
-            [(1, 1)],
-            map(
-                lambda x: next(
-                    (2, int(x[1])) if x[0] == 'addx' else (1, 0)
-                    for x in [x.strip().split()]
+print(list(
+    f(x)
+    for d in [defaultdict(lambda: None,
+        accumulate(
+            chain(
+                [(1, 1)],
+                map(
+                    lambda x: next(
+                        (2, int(x[1])) if x[0] == 'addx' else (1, 0)
+                        for x in [x.strip().split()]
+                    ),
+                    list(input())
                 ),
-                list(input())
+                [(1, 1024)]
             ),
-            [(1, 1024)]
-        ),
-        lambda s, x: (s[0] + x[0], s[1] + x[1])
-    )
-)
-f = lambda k: (k, d[k]) if d[k] else f(k - 1)
-print(sum(map(
-    lambda x: (math.ceil((x[0]-20)/40)+0.5)*40 * x[1],
-    takewhile(lambda x: x[1] < 1024, (f(x) for x in count(20, 40)))
-)))
+            lambda s, x: (s[0] + x[0], s[1] + x[1])
+        ))
+    ]
+    for x in count(20, 40) if f(x) < 1024
+    for f in ((f := lambda k: d[k] if d[k] else f(k - 1)))
+))
 
 # print(sum(list(map(
 #     lambda x: next(
