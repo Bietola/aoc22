fe4961a0883d236658bf050fe56ec600f393d2a8
Bietola <dincio.montesi@gmail.com>
2023-01-31 13:02:45 +0100

    Collapse with step_by

--- rust/p5/src/main.rs
+++ rust/p5/src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
 use ndarray::{Array2, s};
 use regex::Regex;
-use std::{iter, error::Error};
+use std::iter;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let inp = fs::read_to_string("assets/input").unwrap();
     let inp: Vec<_> = inp.split("\n").collect();
     let str_stk: Vec<_> = inp.iter().take(8).collect();
@@ -17,12 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         (8, 9),
         str_stk
             .iter()
-            .map(|line| line.chars()
-                .enumerate()
-                .filter_map(|(i, c)|
-                    i.checked_sub(1).filter(|x| x % 4 == 0).map(|_| c)
-                )
-            )
+            .map(|line| line.chars().skip(1).step_by(4))
             .flatten()
             .collect::<Vec<_>>()
     ).unwrap().reversed_axes().slice_move(s![.., ..;-1]);
@@ -40,12 +35,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     for cmd in prg {
         let get_caps = |n| cmd.as_ref().unwrap().get(n).unwrap()
             .as_str().parse::<usize>().unwrap();
-        let s = get_caps(1);
-        let n = get_caps(2);
-        let d = get_caps(3);
-
-        let s = s - 1;
-        let d = d - 1;
+        let n = get_caps(1);
+        let s = get_caps(2) - 1;
+        let d = get_caps(3) - 1;
 
         let mv: Vec<_> = iter::repeat_with(|| stk[s].pop().unwrap())
             .take(n)
@@ -55,6 +47,4 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!("{}", stk.iter().map(|s| &s[s.len()-1..]).collect::<String>());
-
-    Ok(())
 }
